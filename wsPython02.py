e30e7a7ca19660fdf6ee9c859064d29dfc0c6be9
Atsadawut242 <atsadawutphanpat@gmail.com>
2024-06-05 11:32:57 +0700

    ขนาดของสนามหญ้า

---
+++ wsPython02.py
@@ -0,0 +1,13 @@
+def calculate_grass_cutting_cast(width, length, rate_per_sqm):
+    area = width * length
+    total_cost = area * rate_per_sqm
+    return total_cost
+
+x = float(input("โปรดระบุความกว้างของสนามหญ้า (เมตร)"))
+y = float(input("โปรดระบุความยาวของสนามหญ้า (เมตร)"))
+b = float(input("โปรดระบุราคาหญ้าต่อตารางเมตร (บาท)"))
+
+total_cost = calculate_grass_cost(x,y,b)
+print("ราคาตัดหญ้าทั้งหมดคือ {:.2f} บาท ".format(total_cost))
+
+
